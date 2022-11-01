64c47950118471878181395c3acb0f9d7dd150d5
Geoff Walker <86385035+Geoff-Walker@users.noreply.github.com>
2022-11-01 10:55:05 +0000

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -10,11 +10,9 @@ fi
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
 # echo $($PSQL "TRUNCATE teams, games")
-CT=0 #cycle counter
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINGOAL OPPGOAL
 do
-  let CT+=1
-  if [[ $YEAR != "year" ]]
+    if [[ $YEAR != "year" ]]
   then
     WIN_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     OPP_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
@@ -30,6 +28,5 @@ do
     fi
     ITR=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WIN_ID, $OPP_ID, $WINGOAL, $OPPGOAL)")
   fi
-  echo $CT
-done
+ done
 echo -e "COMPLETED Inserts"
